81bcb12ff7600a86b745bc7a9b1370690630db0f
Victor Franco <vfrancohdez1@hotmail.com>
2021-05-07 15:49:54 -0500

    Encontrar subred
    A partir de una ip y un numero de subred, de host o de prefijo, la
    funcion get_subnet() nos proporciona una tupla con la informacion de la
    subred.

--- compute.py
+++ compute.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import math
 def get_class(ip):
     octetos=[int(i) for i in ip.split(".")]
     primeros_bits=bin(octetos[0])[2:].zfill(8)
@@ -28,5 +29,41 @@ def identificar(ip):
     mask=get_mask(ip)
     print("M.RED: "+mask)
 
+def get_subnet(ip,subredes=0,hosts=0,prefijo=0):
+    mask=get_mask(ip)
+    bytes_host=[i for i in mask.split(".") if i=="0"]
+    host_bits=len(bytes_host)*8             #obtener bits disponibles
+    if prefijo>0:                           #si hay un prefijo
+        host_fijos=32-host_bits             #buscar host fijos
+        bits_req=prefijo-host_fijos         #bits_req=bits de la subred
+        subredes=2**bits_req-2              #asignar la subred para ese prefijo
+
+    size=subredes or hosts                  #numero de ips para host o subredes
+    if size>2:                              #si es posible hacer el logaritmo
+        n=math.log(size+2,2)                #host o subredes=2^n-2
+        bits_available=math.ceil(n)         #redondear
+        if host_bits-bits_available<0:      #si no es posible esa particion
+            msg="No es posible"             #mandar mensaje de error
+            return msg
+    else:
+        msg="No es posible"
+        return msg
+    ips=str(2**bits_available-2)             #ips aprox. que solicitamos
+    complement_bits=host_bits-bits_available #bits restantes
+    complement_ips=str(2**complement_bits-2) #ips restantes
+
+    subredes=ips if subredes else complement_ips  #diferenciar ips requeridas
+    hosts=ips if hosts else complement_ips        #con las sobrantes
+
+    return (subredes,hosts)
+
+def calcular(ip,subredes=0,hosts=0,prefijo=0):
+    msg=get_subnet(ip,subredes,hosts,prefijo)
+    print("Subredes: "+msg[0])
+    print("Host: "+msg[1])
+
 if __name__ == "__main__":
-    identificar("190.0.0.0")
+    ip="190.0.0.0"
+    identificar(ip)
+    #calcular(ip,subredes=3)
+    calcular(ip,prefijo=25)
