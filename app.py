9a8ef8d7886eeec67142e8b48a445ebe47e02746
Victor Franco <vfrancohdez1@hotmail.com>
2021-05-10 08:40:38 -0500

    Arreglar texto de lista de hosts
    Ahora cuando se selecciona una subred, cambia el texto del lable de los
    hosts. Muestra el index de la subred actual.

--- app.py
+++ app.py
@@ -2,17 +2,18 @@
 from tkinter import *
 import compute
 import re
+from tkinter import font
 
 option=0
 def set_option(num):
-    global option
+    global option                   #guardar tipo de busqueda
     option=num
-    for i in range(3):
-        if i==option:
+    for i in range(3):              #el boton que fue pulsado
+        if i==option:               #tendra color distinto
             btn[option]['background']=amarillo
         else:
             btn[i]['background']=naranja
-    search_information("")
+    search_information("")          #actualizar informacion mostrada
 
 def search_information(event):
     ip=display1.get()
@@ -31,41 +32,44 @@ def search_information(event):
         label3['text']=""                     #limpiar
         return 0
     if option==0:
-        sub=compute.get_subnet(display1.get(),hosts=int(num))
+        sub=compute.get_subnet(display1.get(),hosts=int(num))      #segun el tipo de busqueda
     elif option==1:
-        sub=compute.get_subnet(display1.get(),subnets=int(num))
+        sub=compute.get_subnet(display1.get(),subnets=int(num))    #obtenemos el calculo de subred
     else:
         sub=compute.get_subnet(display1.get(),prefix=int(num))
-    new_state="Subredes: "+sub[0]+"\n"        
-    new_state+="Host: "+sub[1]+"\n"           
+    new_state="Subredes: "+sub[0]+"\n"
+    new_state+="Host: "+sub[1]+"\n"
     #new_state+="Prefix: "+sub[2]+"\n"
-    submask=compute.get_submask(sub[2])       
-    new_state+="M.subred: "+submask           
-    label3['text']=new_state                  #mostrar informacion
+    submask=compute.get_submask(sub[2])
+    new_state+="M.subred: "+submask
+    label3['text']=new_state                  #mostrar informacion de subred
     for widget in frame3.winfo_children():
         widget.destroy()
     for widget in frame5.winfo_children():
         widget.destroy()
     subnets_=compute.array_subnets(ip,sub[0],sub[2])
     hosts_=compute.array_hosts(subnets_[0],sub[1])
-    new_frame=create_scroll_frame(frame3,len(subnets_))
-    new_frame2=create_scroll_frame(frame5,len(hosts_))
+    label4['text']="{:^32}".format("Lista de host de la subred   1")
+    new_frame=create_scroll_frame(frame3,len(subnets_))     #crear area de scroll para subnets
+    new_frame2=create_scroll_frame(frame5,len(hosts_))      #crear area de scroll para hosts
     for i,subnets in enumerate(subnets_):
         btn1=Button(new_frame,text=f'{i+1} --> {subnets}',command=lambda i=i,hosts=sub[1]:set_subnet(i,subnets_,hosts),background=verde,highlightbackground="#000",highlightthickness=1,cursor="hand1")
         btn1.place(x=0,y=31*i,width=240)
     for i,hosts in enumerate(hosts_):
         btn1=Button(new_frame2,text=f'{i+1} --> {hosts}',background=verde,highlightbackground="#000",highlightthickness=1,cursor="hand1")
-        btn1.place(x=0,y=31*i,width=203)
+        btn1.place(x=0,y=31*i,width=216)
 
 def set_subnet(index,subnets_,hosts):
-    for widget in frame5.winfo_children():
+    for widget in frame5.winfo_children():                             #borrar contenido
         widget.destroy()
-    hosts_=compute.array_hosts(subnets_[int(index)],hosts)
-    new_frame2=create_scroll_frame(frame5,len(hosts_))
+    hosts_=compute.array_hosts(subnets_[int(index)],hosts)             #obtener nuevos datos
+    new_frame2=create_scroll_frame(frame5,len(hosts_))                 #crear nueva area de scroll
     for i,hosts in enumerate(hosts_):
         btn1=Button(new_frame2,text=f'{i+1} --> {hosts}',background=verde,highlightbackground="#000",highlightthickness=1,cursor="hand1")
-        btn1.place(x=0,y=31*i,width=203)
-    
+        btn1.place(x=0,y=31*i,width=216)
+    msg="{:>3}".format(str(index+1))
+    label4['text']="{:^32}".format("Lista de host de la subred "+msg)  #mostrar nuevo mensaje
+
 def create_scroll_frame(frame,size):
     my_canvas=Canvas(frame)
     my_canvas.pack(side=LEFT,fill=BOTH,expand=1)
@@ -76,14 +80,14 @@ def create_scroll_frame(frame,size):
     new_frame=Frame(my_canvas,background=azul,width=300,height=31*int(size))
     new_frame.pack(fill=BOTH, expand=True)
     my_canvas.create_window((0,0),window=new_frame,anchor="nw")
-    return new_frame
+    return new_frame                                                   #devolver frame con scroll
 
 azul="#5597D4"
 naranja="#F98430"
 amarillo="#f9c62f"
 verde="#68A048"
 root=Tk()
-root.title("Calculadora de ips")
+root.title("Calculadora de ips")                                   #definir titulo
 
 app_width=610
 app_height=400
@@ -91,7 +95,7 @@ screen_width=root.winfo_screenwidth()
 screen_height=root.winfo_screenheight()
 app_posx=int(screen_width/2-app_width/2)
 app_posy=int(screen_height/2-app_height/2)
-root.geometry(f"{app_width}x{app_height}+{app_posx}+{app_posy}")
+root.geometry(f"{app_width}x{app_height}+{app_posx}+{app_posy}")   #dimensiones de ventana
 
 root['background']='#075085'
 frame=Frame(root,background=azul,highlightbackground=azul, highlightcolor=azul,highlightthickness=20)
@@ -99,10 +103,10 @@ frame.pack(fill=None, expand=False)
 
 label1=Label(frame,text="Dir IP",background=naranja,highlightbackground="#000",highlightthickness=1)
 label1.grid(row=1,column=0,sticky=W+E,pady=(0,20))
-display1=Entry(frame)
+display1=Entry(frame)                                               #campo de ip
 display1.grid(row=1,column=2,columnspan=6,sticky=W+E,padx=15,pady=(0,20))
 display1.bind("<Return>",search_information)
-display2=Entry(frame)
+display2=Entry(frame)                                               #campo de eleccion subred
 display2.grid(row=3,column=2,columnspan=6,sticky=W+E+N,padx=15,pady=(0,20))
 display2.bind("<Return>",search_information)
 
@@ -111,28 +115,28 @@ frame2.grid(row=3,column=0)
 btn=[]
 for i in range(3):
     btn.append(Button(frame2,text="Host",command=lambda i=i:set_option(i),background=naranja,highlightbackground="#000",highlightthickness=1,cursor="hand1"))
-    btn[i].grid(row=3+i,column=0,sticky=W+E)
+    btn[i].grid(row=3+i,column=0,sticky=W+E)                        #botones de busqueda
 btn[0]['background']=amarillo
 btn[0]['text']="Host"
 btn[1]['text']="Subred"
 btn[2]['text']="Prefijo"
 
 label2=Label(frame,text="\n",background=verde,highlightbackground="#000",highlightthickness=1)
-label2.grid(row=1,column=9,rowspan=2,sticky=W+E+N,pady=(0,20))
+label2.grid(row=1,column=9,rowspan=2,sticky=W+E+N,pady=(0,20))      #paneles de informacion
 label3=Label(frame,text="",background=verde,highlightbackground="#000",highlightthickness=1)
 label3.grid(row=3,column=9,sticky=W+E+S+N)
 
-label8=Label(frame,text="Lista de subredes",highlightbackground="#000",highlightthickness=1)
+list_font=font.Font(family="Consolas",size=9,weight="bold")         #labels de las listas
+label8=Label(frame,text="{:^32}".format("Lista de subredes"),font=list_font,highlightbackground="#000",highlightthickness=1)
 label8.grid(row=6,column=0,columnspan=8,sticky=W+E,padx=(0,15),pady=20)
-
-label4=Label(frame,text="  Lista de host de la subred 200  ",highlightbackground="#000",highlightthickness=1)
+label4=Label(frame,text="{:^32}".format("Lista de host de la subred   1"),font=list_font,highlightbackground="#000",highlightthickness=1)
 label4.grid(row=6,column=9,sticky=W+E)
 
-frame3=Frame(frame)
+frame3=Frame(frame)                                                 #panel subredes
 frame3.grid(row=7,column=0,columnspan=8,sticky=W+E,padx=(0,15))
 frame3.columnconfigure(0, weight = 1)
 
-frame5=Frame(frame)
+frame5=Frame(frame)                                                 #panel hosts
 frame5.grid(row=7,column=9,columnspan=8,sticky=W+E)
 frame5.columnconfigure(0, weight = 1)
 
